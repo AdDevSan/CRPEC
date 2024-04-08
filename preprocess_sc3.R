de7af5ac36017d943334375e2c11fd8732f0901c
AdDevSan <adriel.d.sandji@gmail.com>
2024-04-08 20:48:56 +0800

    working R preprocess

--- preprocess_sc3.R
+++ preprocess_sc3.R
@@ -27,8 +27,42 @@ sce <- SingleCellExperiment(assays = List(counts = matrix))
 
 
 # TODO: PREPROCESS ACCORDING TO EXISTING PIPELINE
+library(scater)
+library(scran)
 
-# TODO: for each loop run, get cample_200 file ending with loop run from sample.200.directory
+# Quality control - Filtering cells and genes
+min_genes_per_cell <- 200
+min_cells_per_gene <- 3
+
+# Filter out cells with fewer than min_genes_per_cell
+sce <- sce[, sum(counts(sce)) >= min_genes_per_cell]
+
+# Filter out genes detected in fewer than min_cells_per_gene cells
+sce <- sce[rowSums(counts(sce) > 0) >= min_cells_per_gene, ]
+
+# Compute size factors to normalize the library sizes across cells
+sce <- computeMedianFactors(sce)
+
+# Normalize the counts using the size factors
+# Compute size factors for normalization
+# clusters <- quickCluster(sce)
+# sce <- computeSumFactors(sce, clusters=clusters)
+# sce <- normalizeCounts(sce)
+
+# Log transformation - Apply log transformation
+# Access the counts assay directly
+counts <- assay(sce, "counts")
+
+# Apply the log1p transformation to the counts
+log_counts <- log1p(counts)
+
+# Update the assay data with the transformed counts
+assay(sce, "logcounts") <- log_counts
+
+
+
+
+# TODO: for each loop run, get sample_200 file ending with loop run from sample.200.directory
   # TODO: subset sce with sample_200 into sce_200
   # TODO: predict SC3 cluster into initial_cluster
     # TODO: get optimal cluster
